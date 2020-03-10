ef94ea7133c47ed8df79b5f0415647da72f55350
Librante <natalja.librante@gmail.com>
2020-03-10 16:11:20 +0200

    All scenario dry run

--- src/test/java/StepDefinitions/StepDefinitions.java
+++ src/test/java/StepDefinitions/StepDefinitions.java
@@ -19,11 +19,12 @@ public class StepDefinitions {
 RequestsImpl request = new RequestsImpl();
 DataStorage dataStorage = new DataStorage();
 
-    @Given("the user shuffles the new pile of cards")
-    public void theUserShufflesTheNewPileOfCards() {
+    @Given("the user shuffles the new deck of cards")
+    public void theUserShufflesTheNewDeckOfCards() {
         Response response = request.GetRequest(Constants.BASE_URL+ Constants.NEW_DECK);
         String desk_id = (String) request.receiveJsonValueFromResponse(response,"deck_id");
         dataStorage.setDeck_id(desk_id);
+        System.out.println(dataStorage.getDeck_id());
     }
 
     @When("the user deals {string} cards")
@@ -74,10 +75,17 @@ DataStorage dataStorage = new DataStorage();
 
     @Then("the player have a new pile with {string}")
     public void thePlayerHaveANewPileWithOnly(String pileName) {
-        String cardsInPile = "";
-        switch (pileName){
-            case("Aces"):
+        String cardsInPile;
+        switch (pileName) {
+            case ("Aces"):
                 cardsInPile = Constants.ALL_ACES;
+        break;
+            case ("allCards"):
+                cardsInPile = Constants.ALL_CARDS;
+                break;
+            default:
+                cardsInPile = pileName;
+                pileName = "randomPile";
         }
         Map<String, String> queryParams = new HashMap();
         queryParams.put("cards", cardsInPile);
@@ -86,12 +94,13 @@ DataStorage dataStorage = new DataStorage();
 
         Response response = request.GetRequestWithQueryParam(Constants.BASE_URL+ Constants.CREATE_SPECIFIC_PILE, queryParams);
         dataStorage.setResponse(response);
+        dataStorage.setPileName(pileName);
         System.out.println(response.asString());
 
     }
 
-    @Then("the player sees only {string} in dealing")
-    public void thePlayerSeesOnlyInDealing(String expectedCards) {
+    @Then("the player sees only {string} in dealing of {string} pile")
+    public void thePlayerSeesOnlyInDealing(String expectedCards, String pileName) {
         String cardsInPile = "";
         switch (expectedCards) {
             case ("Aces"):
@@ -100,13 +109,46 @@ DataStorage dataStorage = new DataStorage();
         String[] strParts = cardsInPile.split(",");
         ArrayList<String> cardsReceivedInDeal = new ArrayList<>(Arrays.asList(strParts));
 
-        List<Object> values = request.receiveJsonValuesAsListFromResponse(dataStorage.getResponse(),"piles.Aces.cards.value");
-        List<Object> codes = request.receiveJsonValuesAsListFromResponse(dataStorage.getResponse(),"piles.Aces.cards.code");
+        List<Object> values = request.receiveJsonValuesAsListFromResponse(dataStorage.getResponse(),"piles."+pileName+".cards.value");
+        List<Object> codes = request.receiveJsonValuesAsListFromResponse(dataStorage.getResponse(),"piles."+pileName+".cards.code");
         assertThat(cardsReceivedInDeal, is(codes));
         assertTrue (values.stream().allMatch(v -> v.equals("ACE")));
         System.out.println(values);
         System.out.println(codes);
 
     }
+
+
+    @When("the user draws {string} cards from the {string} of {string} pile")
+    public void theUserDrawsCardsFromTheOfPile(String cardsToDraw, String fromWhereToDrawCards, String pileName) {
+        Map<String, String> queryParams = new HashMap();
+        queryParams.put("cards", cardsToDraw);
+        queryParams.put("deck_id", dataStorage.getDeck_id());
+        queryParams.put("whereToDraw",fromWhereToDrawCards);
+        queryParams.put("pileName", pileName);
+        Response response = request.GetRequestWithQueryParam(Constants.BASE_URL+ Constants.DRAW_FROM_THE_DECK_BOTTOM, queryParams);
+
+        System.out.println(response.asString());
     }
+
+    @Then("the pile {string} contains <{int}> cards")
+    public void thePileContainsCards(String pileName, int countOfCards) {
+
+        int cardsRemains = (Integer) request.receiveJsonValueFromResponse(dataStorage.getResponse(),"piles."+pileName+".remaining");
+        assertThat(cardsRemains, is(countOfCards));
+    }
+
+    @And("the pile {string} do not contains {string} cards in it")
+    public void thePileAllCardsDoNotContainsCardsInIt(String pileName, String cardsToBeExcluded) {
+        List<Object> codes = request.receiveJsonValuesAsListFromResponse(dataStorage.getResponse(),"piles."+pileName+".cards.code");
+        System.out.println(codes);
+        String[] strParts = cardsToBeExcluded.split(",");
+        ArrayList<String> cardsReceivedInDeal = new ArrayList<>(Arrays.asList(strParts));
+        System.out.println(cardsReceivedInDeal);
+        for (String cardEx: cardsReceivedInDeal){
+        assertTrue("For " +cardEx + "something get wrong",
+                codes.stream().noneMatch(v -> v.equals(cardEx)));
+ }
+    }
+}
 
--- src/test/resources/features/cards.feature
+++ src/test/resources/features/cards.feature
@@ -1,7 +1,7 @@
 Feature: Cards
 
   Scenario Outline: User draws a card from the deck and checks remaining cards
-    Given the user shuffles the new pile of cards
+    Given the user shuffles the new deck of cards
     When the user deals "<number>" cards
     Then the deck contains <remainingNumber> cards
     Examples:
@@ -9,10 +9,25 @@ Feature: Cards
       | 3      | 49              |
       | 27     | 25              |
       | 42     | 10              |
-@wip
-    Scenario: User creates deck containing only aces and player can receive only aces from this deck
-  Given the user creates the new deck of cards with only "Aces"
-      And the user deals "4" cards
-      And the player have a new pile with "Aces"
-      When the player reviews his cards in "Aces" pile
-      Then the player sees only "Aces" in dealing
+
+  Scenario: User creates deck containing only aces and player can receive only aces from this deck
+    Given the user creates the new deck of cards with only "Aces"
+    And the user deals "4" cards
+    And the player have a new pile with "Aces"
+    When the player reviews his cards in "Aces" pile
+    Then the player sees only "Aces" in dealing of "Aces" pile
+
+    #You can draw specific cards only from a pile, not from the deck as was described in task
+  @wip
+  Scenario Outline: User draws 5 specific cards from a bottom of checks remaining cards in the deck
+    Given the user shuffles the new deck of cards
+    And the user deals "52" cards
+    And the player have a new pile with "allCards"
+    When the user draws "<drawCards>" cards from the "bottom" of "allCards" pile
+    When the player reviews his cards in "allCards" pile
+    Then the pile "allCards" contains <47> cards
+    And the pile "allCards" do not contains "<drawCards>" cards in it
+    Examples:
+      | drawCards      |
+      | 0C,6S,AD,KS,QS |
+
